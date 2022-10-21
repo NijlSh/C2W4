1d2c9e791cc45aef6b0a8c42d6042ee1500780bd
NijlSh <75314884+NijlSh@users.noreply.github.com>
2022-10-22 01:17:48 +0300

    Update DialogFinder.cpp

--- Project/DialogFinder.cpp
+++ Project/DialogFinder.cpp
@@ -9,7 +9,7 @@ void DialogFinder::FindDialogs()
 
 	for (int i = 1; i < static_cast<int>(text.size()); i++)
 	{
-		if (text[i - 1] == '\n' && text[i] == '–' && text[i + 1] == ' ')
+		if (text[i - 1] == '\n' && text[i] == '—' && text[i + 1] == ' ')
 		{	
 			if (!((text[i + 2] == '\n' || text[i + 2] == ' ')))
 			{
@@ -23,7 +23,7 @@ void DialogFinder::FindDialogs()
 		if (isDialog)
 			tempDialog += text[i];
 
-		if ((text[i] == '\n' && text[i] != '–' || isIncorrectLine)
+		if (text[i] == '\n' && text[i] != '—' || isIncorrectLine)
 		{
 			isDialog = false;
 			isIncorrectLine = false;
