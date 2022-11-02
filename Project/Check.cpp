c7732e2d44a4b12fcb33c931000c69fb8126b161
NijlSh <75314884+NijlSh@users.noreply.github.com>
2022-11-02 07:22:12 +0300

    Add files via upload

--- Project/Check.cpp
+++ Project/Check.cpp
@@ -1,32 +1,79 @@
 #include "Check.h"
+
 int CheckMenu(int items)
 {
-	std::string value_input;
-	int value;
+	std::cout << "Ввод: ";
 	while (true)
 	{
-
-		std::cout << "Ввод: ";
-		std::getline(std::cin, value_input);
-		std::cout << std::endl;
-		std::cin.sync();
-
+		int value = 0;
 		try
 		{
-			value = stoi(value_input);
+			value = GetPositiveInt();
 			if (value <= items && value >= 1)
 				return value;
 			else
 				throw value;
 		}
-		catch (std::exception&)
-		{
-
-			std::cout << "Не существующий пункт меню. Повторите ввод." << std::endl;
-		}
 		catch (int ex_value)
 		{
-			std::cout << ex_value << "Не существующий пункт меню. Повторите ввод." << std::endl;
+			std::cout << std::endl;
+			std::cout << ex_value << " – не существующий пункт меню. Повторите ввод: ";
+			return 0;
 		}
 	}
+}
+
+template<typename T>
+T GetValue()
+{
+	T temp = static_cast<T>(0.0);
+	do
+	{
+		try
+		{
+			std::cin >> temp;
+			std::cout << std::endl;
+			if (std::cin.peek() != '\n')
+				throw std::exception("IncorrectValue");
+			return temp;
+		}
+		catch (const std::exception&)
+		{
+			std::cout << "Введены некорректные данные. Повторите ввод: ";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	} while (true);
+}
+
+template <typename T>
+T GetPositiveValue()
+{
+	T temp = static_cast<T>(0.0);
+	do
+	{
+		try
+		{
+			temp = GetValue<T>();
+			if (temp <= 0) throw error_;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			return temp;
+		}
+		catch (const int&)
+		{
+			std::cout << "Число должно быть положительным. Повторите ввод: ";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	} while (true);
+}
+
+int GetPositiveInt()
+{
+	return GetPositiveValue<int>();
+}
+
+int GetInt()
+{
+	return GetValue<int>();
 }
--- Project/Check.h
+++ Project/Check.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <cstdlib>
 
+const int error_ = 1;
+
+enum numberOfChoice
+{
+	two = 2,
+	three = 3
+};
 
 int CheckMenu(int items);
+int GetPositiveInt();
+int GetInt();
